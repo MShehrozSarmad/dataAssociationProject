57425b9b506eca0a9046209376dc3d3b2c302593
Muhammad Shehroz Sarmad <shehrozm107@gmail.com>
2024-08-08 19:52:54 +0500

    1st part

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const userModel = require('./models/user')
 
 const app = express();
 
@@ -16,8 +17,57 @@ app.get('/', (req, res) => {
 
 app.post('/createaccount', async (req, res) => {
     const {name, username, age, email, password} = req.body;
-    console.log(email);
-    res.send('done')
+    const user = await userModel.findOne({email});
+    if(user) return res.send('user already exist');
+
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, async (err, hash) => {
+            let user = await userModel.create({
+                name, email, age, username, password: hash
+            })
+            const token = jwt.sign({email, userid: user._id}, 'secretKey');
+            res.cookie('token', token);
+            res.status(200).send('account created successfully ✅');
+        })
+    })
 })
+
+app.get('/login', async(req, res) => {
+    res.render('login');
+})
+
+app.post('/login', async (req, res) => {
+    const {email, password} = req.body;
+
+    const user = await userModel.findOne({email});
+    if(!user) return res.send('user not found');
+
+    bcrypt.compare(password, user.password, (err, result) => {
+        if(result){
+            const token = jwt.sign({email, userid: user._id}, 'secretKey');
+            res.cookie('token', token);
+            res.status(200).send('You are logged in successfully ✅');
+        } else res.send('invalid cridentials ⚠️')
+    })
+})
+
+app.get('/logout', (req, res) => {
+    res.cookie('token', '');
+    res.send('Sad to see you go :(');
+})
+
+app.get('/post', isLoggedIn, (req, res) => {
+    console.log(req.user);
+    res.send('done');
+})
+
+function isLoggedIn(req, res, next){
+    if(req.cookies.token == '') return res.send('You must be logged in first');
+    else{
+        const data = jwt.verify(req.cookies.token, 'secretKey');
+        req.user = data;
+        next();
+    }
+}
 
 app.listen(3000)
